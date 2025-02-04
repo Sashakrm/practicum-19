69e997e067c9879bcad1950b5c2a8473f47c913e
Sashakrm <sasakramch@gmail.com>
2025-02-04 20:28:11 +0300

    Add files via upload

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,13 +12,16 @@ namespace ConsoleApp1
         static void Cicle()
         {
             int n = 4;
+            int sum = 1;
             int x = 1;
             Console.Write("1 ");
             for(int a=2; a<=5;a++)
             {
                 x = x + n;
                 Console.Write($"{x} ");
+                sum = sum + x;
             }
+            Console.WriteLine($", sum={sum}");
         }
     }
 }

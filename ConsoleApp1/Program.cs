cce8576c747a95e92db50b068d20595ef5b2d9cd
Sashakrm <sasakramch@gmail.com>
2025-02-04 20:24:18 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cicle();
+            Console.Read();
+        }
+        static void Cicle()
+        {
+            int n = 4;
+            int x = 1;
+            Console.Write("1 ");
+            for(int a=2; a<=5;a++)
+            {
+                x = x + n;
+                Console.Write($"{x} ");
+            }
+        }
+    }
+}
